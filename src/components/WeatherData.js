5aea2db966d1dfd2fa90880da61f6d0e1056a0d4
pmaciej <pmaciej@users.noreply.github.com>
2021-04-27 02:04:14 +0200

    up

---
+++ src/components/WeatherData.js
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+import convertKelvinTemp from '../helpers/convertKelvins';
+import Context from '../Context';
+import Weather from './Weather';
+
+const WeatherData = (day) => {
+  const {location} = useContext(Context);
+  const {
+    temp: {min: tempMin, max: tempMax},
+    weather: [{icon}],
+    dt,
+    wind_speed,
+  } = day.day;
+  
+  const convertedDate = new Date(dt * 1000);
+  const date = convertedDate.toISOString().slice(0, 10);
+  const dayName = convertedDate.toLocaleString('en-us', {weekday: 'long'});
+  const tempData = convertKelvinTemp(tempMin, tempMax);
+
+  return (
+    <Weather
+      tempData={tempData}
+      windSpeed={wind_speed}
+      icon={icon}
+      date={date}
+      dayName={dayName}
+      location={location}
+    />
+  );
+};
+
+export default WeatherData;
